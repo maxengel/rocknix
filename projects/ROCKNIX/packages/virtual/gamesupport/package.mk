e5d555cd20661d2145f1139588757be02b0e5db7
Max Engel <max.engel@me.com>
2025-08-12 03:35:53 -0400

    Add initial configuration options for GENERIC_X64 device
    - Created a new options file for the GENERIC_X64 device.
    - Disabled 32-bit support to enforce 64-bit only builds.
    - Set the display server to X11 for compatibility with X11 applications.

--- projects/ROCKNIX/packages/virtual/gamesupport/package.mk
+++ projects/ROCKNIX/packages/virtual/gamesupport/package.mk
@@ -9,8 +9,15 @@ PKG_LONGDESC="Game support software metapackage."
 
 PKG_GAMESUPPORT="sixaxis rocknix-hotkey jstest-sdl gamecontrollerdb sdljoytest sdltouchtest control-gen mangohud"
 
-# rocknix-touchscreen-keyboard requires sway
-[[ "${WINDOWMANAGER}" = "swaywm-env" ]] && PKG_GAMESUPPORT+=" rocknix-touchscreen-keyboard"
+# Add touchscreen keyboard for devices that need it, exclude for desktop/x64
+case "${DEVICE}" in
+  GENERIC_X64)
+    # Desktop/laptop systems typically use physical keyboards
+    ;;
+  *)
+    PKG_GAMESUPPORT="${PKG_GAMESUPPORT} rocknix-touchscreen-keyboard"
+    ;;
+esac
 
 PKG_DEPENDS_TARGET="${PKG_GAMESUPPORT}"
 
