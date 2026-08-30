8f0faf9d7db492d6d8f1f8bca856c3534b41c083
Max Engel <max.engel@me.com>
2026-08-30 01:18:58 +0000

    libpsl: use ICU for IDN, not libidn2

--- packages/web/libpsl/package.mk
+++ packages/web/libpsl/package.mk
@@ -7,11 +7,13 @@ PKG_SHA256="1dcc9ceae8b128f3c0b3f654decd0e1e891afc6ff81098f227ef260449dae208"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/rockdaboot/libpsl"
 PKG_URL="https://github.com/rockdaboot/libpsl/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libidn2"
+PKG_DEPENDS_TARGET="toolchain icu"
 PKG_LONGDESC="Public Suffix List library, used by libsoup to scope cookies to registrable domains."
 
 pre_configure_target() {
-  PKG_MESON_OPTS_TARGET="-Druntime=libidn2 \
+  # ICU rather than libidn2: it is already in the tree and in the image,
+  # and libidn2 would drag in libunistring, which is not.
+  PKG_MESON_OPTS_TARGET="-Druntime=libicu \
                          -Dbuiltin=true \
                          -Ddocs=false \
                          -Dtests=false"
