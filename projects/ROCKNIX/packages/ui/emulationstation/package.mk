b28b542d372a7c03a8a021e17cf45793bfa31f68
Max Engel <max.engel@me.com>
2026-08-31 19:42:08 +0000

    emulationstation: bump package

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="db40a836eed793550c9faba11ac5a63710acf897"
+PKG_VERSION="ea750c82dc3e5d605b79dcbef42642f0fd645707"
 PKG_GIT_CLONE_BRANCH="test/qa-integration"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/maxengel/emulationstation-next"
