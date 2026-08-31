f751f78375389f41a009c0cbe0f66f9cbeba8f11
Max Engel <max.engel@me.com>
2026-08-31 04:48:41 +0000

    emulationstation: bump package
    Picks up the on-device sign-in page and the high-contrast QR.

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="c8645756ab0e7459be9b71bcdbaf1c7a93203d7f"
+PKG_VERSION="fa713cced42d5a9da6fcf90e51b55b572c9d76b6"
 PKG_GIT_CLONE_BRANCH="test/qa-integration"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/maxengel/emulationstation-next"
