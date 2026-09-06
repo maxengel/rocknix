d162f80bc0ed9e5587a2f318ded7e50dee571101
Max Engel <max.engel@me.com>
2026-09-06 06:51:06 +0000

    emulationstation: bump package
    5dd7f7f04: one line under SCRAPED GAME CONTENT (D-UI-023).

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="6ce655b32013408437162a2dc79958be365d7c6f"
+PKG_VERSION="5dd7f7f048d99c9dbf15ebd285cc780147a28f7a"
 PKG_GIT_CLONE_BRANCH="test/qa-integration"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/maxengel/emulationstation-next"
