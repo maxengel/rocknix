744a19e3af97f143a9d75ed5a0590c29de289602
Max Engel <max.engel@me.com>
2026-09-06 06:30:14 +0000

    emulationstation: bump package
    6ce655b32: scraped game content is a switch; systems pages per
    direction, compared by file (#76).

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="a13966a2184035456064c0af3832353195cedd2b"
+PKG_VERSION="6ce655b32013408437162a2dc79958be365d7c6f"
 PKG_GIT_CLONE_BRANCH="test/qa-integration"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/maxengel/emulationstation-next"
