748ffd41b81ae06c038e07aaf0783ca1b48eb6e5
Max Engel <max.engel@me.com>
2026-09-08 20:47:07 +0000

    emulationstation: bump to dbb93c567f -- ASCII comments so the i18n step builds

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="7a70af0184e925d90116ba95a290f5129d9cd482"
+PKG_VERSION="dbb93c567fa6535712998f9169ec1176bf20c717"
 PKG_GIT_CLONE_BRANCH="test/qa-integration"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/maxengel/emulationstation-next"
